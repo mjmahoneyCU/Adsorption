edbe93ddccf73aa67fc0124efcf76ef88e25f92c
mjmahoneyCU <melissa.mahoney@colorado.edu>
2025-03-28 18:19:21 -0600

    Update predictionsfinal.py

--- predictionsfinal.py
+++ predictionsfinal.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp, cumtrapz, simpson
+from scipy.integrate import solve_ivp, cumulative_trapezoid, simpson
 import pandas as pd
 
 # Page setup
